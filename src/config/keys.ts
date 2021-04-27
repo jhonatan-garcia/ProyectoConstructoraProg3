b46b401a7f8b58eea544609ac015baa99a7b7a2b
Jhonatan David Garcia <jhonatan.1701811787@ucaldas.edu.co>
2021-04-26 21:12:27 -0500

    ...

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,5 +1,5 @@
 export namespace Keys {
-  export const origeCorreoElectronico = 'alberth.1701810797@ucaldas.edu.co';
+  export const origeCorreoElectronico = 'jhonatan.1701811787@ucaldas.edu.co';
   export const asuntoNuevoUsuario = '[Nuevo usuario] Mensaje de bienvenida';
   export const tiempoVencimientoJWT = Math.floor(Date.now() / 1000) + (60 * 60 * 12);
   export const claveSecretaJWT = 'jwt@Prog3*';
